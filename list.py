24c76206d8602622864416d4ec51480d7640a030
dintibasumatary08 <dintibasumatary547@gmail.com>
2026-07-09 10:32:16 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,22 @@
+def student(rollno, name,):
+    print("rollno",rollno)
+    print("name",name)
+    
+    def student(subject):
+        print("subject",subject)
+        
+        def students(classname):
+            print("class",classname)
+            
+            def length(subject):
+                print("length")
+             
+             
+        def main():
+            student(19,"Dinti","DELD","PYTHON")
+            student(20,"Anvi","maths","c++")
+            
+            return student("rollno",rollno)
+            return student("name",name)
+            return student("subject",subject)
+            return classname("class",classname)           
